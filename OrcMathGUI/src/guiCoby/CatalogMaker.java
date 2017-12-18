0f8b7791d18f0164fca82035acd616ac99c5047b
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-12-18 11:33:57 -0500

    s

--- OrcMathGUI/src/guiCoby/CatalogMaker.java
+++ OrcMathGUI/src/guiCoby/CatalogMaker.java
@@ -1,14 +1,56 @@
 package guiCoby;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CatalogMaker {
 
-	private ArrayList<Book> books;
+	private static ArrayList<Book> books;
+	public static Scanner in;
+	private static boolean done = false;
 	
 	public static void main(String[] args) {
+		in = new Scanner(System.in);
 		CatalogMaker maker = new CatalogMaker();
-		System.out.println(maker.getCSVContent());
+		while(!done) {
+			String newName = getName();
+			String newAuthor = getAuthor();
+			String newGenre = getGenre();
+			int newCount = getCount();
+			int newPrice = getPrice();
+			Book newOne = new Book(newPrice, newCount, newName, newAuthor, newGenre);
+			books.add(1,newOne);
+			System.out.println(maker.getCSVContent());
+			System.out.println("Are you done adding?");
+			if(in.nextLine().equals("yes")) {
+				break;
+			}
+		}
+	}
+	
+	public static String getName() {
+		System.out.println("What is the name of the book you want to add?");
+		return in.nextLine();
+	}
+
+	private static int getPrice() {
+		System.out.println("How much does this book cost?");
+		return Integer.valueOf(in.nextLine());
+	}
+
+	private static int getCount() {
+		System.out.println("How many pages does this book have?");
+		return Integer.valueOf(in.nextLine());
+	}
+
+	private static String getGenre() {
+		System.out.println("What is the genre of this book?");
+		return in.nextLine();
+	}
+
+	private static String getAuthor() {
+		System.out.println("Who wrote this book?");
+		return in.nextLine();
 	}
 
 	public CatalogMaker() {
